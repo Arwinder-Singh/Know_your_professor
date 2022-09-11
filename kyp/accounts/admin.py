5896e892beaa15eec4b172249a0e3e6aa8209cbd
Arwinder-Singh <dhillonwind@gmail.com>
2022-09-11 23:05:13 +0530

    updated Model

--- kyp/accounts/admin.py
+++ kyp/accounts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,Tag,Rating,AvgRating
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Tag)
+admin.site.register(Rating)
+admin.site.register(AvgRating)
--- kyp/accounts/models.py
+++ kyp/accounts/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+        tag_name=models.CharField(max_length=30)
+        
+        
+        def __str__(self):
+            return self.tag_name
+        
 class Profile(models.Model):
     name=models.CharField(max_length=30)
     department=models.CharField(max_length=10)
@@ -8,7 +15,25 @@ class Profile(models.Model):
     post=models.CharField(max_length=20)
     pic=models.ImageField()
     email=models.EmailField()
-    ph_num=models.IntegerField(max_length=12,null=True)
+    ph_num=models.IntegerField(null=True)
+    tag=models.ManyToManyField(Tag)
     
     def __str__(self):
         return self.name
+    
+class Rating(models.Model):
+    R_tag=models.ForeignKey(Tag,on_delete=models.CASCADE,related_name='tag')
+    R_profile=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='profile')
+    tag_rating=models.FloatField(null=True,blank=True,default=None)
+    
+class AvgRating(models.Model):
+    Avg_tag=models.ForeignKey(Tag,on_delete=models.CASCADE,related_name='avg_tag')
+    Avg_profile=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='avg_profile')
+    tag_avgRating=models.FloatField(null=True,blank=True,default=None)
+    
+    class Meta:
+        unique_together=[['Avg_tag','Avg_profile']]
+    
+    
+    
+        
