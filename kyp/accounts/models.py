b641f7fbd94ba3130c6c82c58e48a05a2ddfea65
Arwinder-Singh <dhillonwind@gmail.com>
2022-09-11 23:35:45 +0530

    final db

--- kyp/accounts/models.py
+++ kyp/accounts/models.py
@@ -16,23 +16,28 @@ class Profile(models.Model):
     pic=models.ImageField()
     email=models.EmailField()
     ph_num=models.IntegerField(null=True)
-    tag=models.ManyToManyField(Tag,through="AvgRating")
+    tag=models.ManyToManyField(Tag)
     
     def __str__(self):
         return self.name
     
 class Rating(models.Model):
-    R_tag=models.ForeignKey(Tag,on_delete=models.CASCADE,related_name='tag')
-    R_profile=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='profile')
+    tag=models.ForeignKey(Tag,on_delete=models.CASCADE,related_name='tag')
+    profile=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='profile')
     tag_rating=models.FloatField(null=True,blank=True,default=None)
     
+    # def __str__(self):
+    #     return self.profile
+    
 class AvgRating(models.Model):
-    Avg_tag=models.ForeignKey(Tag,on_delete=models.CASCADE,related_name='avg_tag')
-    Avg_profile=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='avg_profile')
+    tag=models.ForeignKey(Tag,on_delete=models.CASCADE,related_name='avg_tag')
+    profile=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='avg_profile')
     tag_avgRating=models.FloatField(null=True,blank=True,default=None)
     
+    # def __str__(self):
+    #     return self.profile
     class Meta:
-        unique_together=[['Avg_tag','Avg_profile']]
+        unique_together=[['tag','profile']]
     
     
     
